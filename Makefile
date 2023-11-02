e719709dcedd72fc6f6d541ae5b621737af6ba7e
shinocoshi <143145359+shinocoshi@users.noreply.github.com>
2023-11-02 06:50:49 +0000

    Avances

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
-bin/compilar : src/main.cpp
-	c++ src/main.cpp -o bin/ejecutable -I include
+bin/main : src/main.cpp include/*.hpp
+	c++ src/main.cpp -o bin/ejecutable -I include -l curses
+
+# ejecutar
+run : bin/ejecutable
+	./bin/ejecutable
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,49 @@
-#include <iostream>
-#include <Mascota.hpp>
+#include <Ventana.hpp>
+#include <Dibujo.hpp>
+#include <curses.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Juego de mascotas" << std::endl;
 
-    Mascota m1("Firulais");
-    m1.Jugar();
-    m1.Jugar();
-    m1.Jugar();
-    std::cout
-    <<m1.DecirNombre() <<"tiene" <<m1.DecirHambre() <<"de hambre";
-    
-    m1.Comer(5);
+    Ventana v;
+
+    Dibujo d1(100, 1, "zombie");
+    Dibujo d2(1, 1, "planta");
+    bool ejecucion = true;
+    while (ejecucion)
+    {
+        // ciclo de actualizacion
+        v.Actualizar();
+        int input = getch();
+        switch (input)
+        {
+        case 'q':
+            ejecucion = false;
+            break;
+        case 'd':
+            d1.AvanzarX(1);
+            break;
+        case 'a':
+            d1.RetrocederX(1);
+            break;
+        case 'w':
+            d1.RetrocederY(1); // Mueve hacia arriba en Y
+            break;
+        case 's':
+            d1.AvanzarY(1); // Mueve hacia abajo en Y
+            break;
+        }
+        // ciclo de dibujo
+        clear();
+        d2.Dibujar();
+        d1.Dibujar();
+
+        // v.Dibujar();
+
+        refresh();
+        usleep(41000); // 24 fps
+    }
+
     return 0;
 }
